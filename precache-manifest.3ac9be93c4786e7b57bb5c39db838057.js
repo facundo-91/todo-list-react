321fe44788d1b142325548eabaad38e4544b17b4
Facundo Benitez <facundob91@gmail.com>
2020-03-15 19:35:24 -0300

    Updates

---
+++ precache-manifest.3ac9be93c4786e7b57bb5c39db838057.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0831d88197e9320c1dfb48aff39cfba6",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "1266d598a002f6066141",
+    "url": "/todo-list-react/static/css/main.965d9697.chunk.css"
+  },
+  {
+    "revision": "57908c61e41b6bf71ed2",
+    "url": "/todo-list-react/static/js/2.adeb8498.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo-list-react/static/js/2.adeb8498.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1266d598a002f6066141",
+    "url": "/todo-list-react/static/js/main.e5ad60c9.chunk.js"
+  },
+  {
+    "revision": "165674716763adc454e7",
+    "url": "/todo-list-react/static/js/runtime-main.b6dc68da.js"
+  }
+]);
